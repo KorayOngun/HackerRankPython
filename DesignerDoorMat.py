467e5694ab7a0d2e19090a93fde50f583ddbdabb
KorayOngun <90770831+KorayOngun@users.noreply.github.com>
2022-12-14 13:51:57 +0300

    git init

---
+++ DesignerDoorMat.py
@@ -0,0 +1,73 @@
+"""""""""
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+Input Format
+
+A single line containing the space separated values of  and .
+
+Constraints
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+
+9 27
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+
+"""""""""
+
+n, m = map(int, input().split())
+list1 = []
+
+for i in range(0,n):
+        c = int((m - (3+(i*6)))/2)
+        nkt = int((((i+1)*2))-1)
+        if c > 0 :
+                s = "-"*c+".|."*nkt+"-"*c
+                list1.append(s)
+                print(s)
+        elif c==0:
+                a = int((m-7)/2)
+                s = "-"*a+'WELCOME'+"-"*a
+                print(s)
+        elif c < 0:
+                print(list1[len(list1)-1])
+                list1.pop()
